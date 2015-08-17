13e4ed7003f43e079b6c7e2d081b2a5258150ecf
chen_jw <chen_jw@Ctrip.com>
2015-08-17 19:19:21 +0800

    Init

---
+++ MEFTest/MEFTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
